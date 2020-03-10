349514dfe0c79c65a162a5ec203688fa70648b22
wangyu <wy_10808@163.com>
2020-03-10 23:14:42 +0800

    2

---
+++ my-shop-service/itqf-cart-service/src/main/java/com/itqf/ItqfCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itqf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItqfCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItqfCartServiceApplication.class, args);
+    }
+
+}
---
+++ my-shop-service/itqf-staticsource-service/src/main/java/com/itqf/ItqfStaticsourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itqf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItqfStaticsourceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItqfStaticsourceServiceApplication.class, args);
+    }
+
+}
